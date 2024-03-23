50c0e1fdd5f4cf52bacec77bdb4914ce86be4d2b
Deavanzack <daffafadillah564@gmail.com>
2024-03-24 02:49:58 +0700

    menambahkan class SayaTubeVideo

---
+++ TPmodul6/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPmodul6
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            this.title = title;
+            this.playCount = 0;
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+        }
+
+        public void IncreasePlayCount(int angka)
+        {
+            this.playCount = this.playCount + angka;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine(this.id);
+            Console.WriteLine(this.title); 
+            Console.WriteLine(this.playCount);  
+        }
+    }
+}
